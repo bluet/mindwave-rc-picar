41ab939e8c82ee64be6f61bc8e9055f054a39179
BlueT - Matthew Lien - 練喆明 <BlueT@BlueT.org>
2016-06-24 17:17:43 +0800

    add msg in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,16 @@
+echo "Generate Ports"
 echo 6 > /sys/class/gpio/export
 echo 13 > /sys/class/gpio/export
 echo 19 > /sys/class/gpio/export
 echo 26 > /sys/class/gpio/export
 
+echo "Set direction to OUT"
 echo out > /sys/class/gpio/gpio6/direction
 echo out > /sys/class/gpio/gpio13/direction
 echo out > /sys/class/gpio/gpio19/direction
 echo out > /sys/class/gpio/gpio26/direction
 
+echo "Set values to 0"
 echo 0 > /sys/class/gpio/gpio6/value
 echo 0 > /sys/class/gpio/gpio13/value
 echo 0 > /sys/class/gpio/gpio19/value
